e5c80045ed3f24233f250d2ad166bce75fcec90b
DAITADOKORO <bangasa0101@gmail.com>
2019-12-08 21:48:03 +0900

    created admin_user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
+  devise_for :admin_users, controllers: {
+    sessions: 'admin/admin_users/sessions',
+    passwords: 'admin/admin_users/passwords',
+    registrations: 'admin/admin_users/registrations'
+  }
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
