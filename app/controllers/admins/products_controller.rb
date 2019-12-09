cf546a52a05d852e5eee35f3fc9aff9711276052
DAITADOKORO <bangasa0101@gmail.com>
2019-12-09 20:53:35 +0900

    finished admins_products_index

--- app/controllers/admins/products_controller.rb
+++ app/controllers/admins/products_controller.rb
@@ -3,7 +3,8 @@ class Admins::ProductsController < ApplicationController
 layout "admin"
 
   def index
-    @admins_products = Product.all
+    @admins_products = Product.page(params[:page]).reverse_order
+
   end
 
   def show
