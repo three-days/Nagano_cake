5331f13b35c3eaff966ff415a12688bf87cc38ae
SAKURAI-S <japan.coffee.pm@gmail.com>
2019-12-10 10:04:38 +0000

    userページ一時的に終わり

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
+
+	def index
+	end
 	def show
+		@users = current_user
 		
-		
+	end
+	def edit
+		@user = User.find(params[:id])
 	end
 
 	def destroy
