07f7f16b991615b6e636d1f196aaf79a64734ebd
DAITADOKORO <bangasa0101@gmail.com>
2019-12-11 14:48:31 +0900

    came back

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -1,6 +1,8 @@
 class Admins::UsersController < ApplicationController
 
-layout "admin"
+# before_action :authenticate_user!
+
+ layout "admin"
 
   def index
     @admins_users = User.all
@@ -8,19 +10,27 @@ layout "admin"
 
   def show
     @admins_user = User.find(params[:id])
-    @admins_order = Order.find(@admins_user.order_id)
+    @orders = @admins_user.orders.page(params[:page]).reverse_order
   end
 
   def edit
+    @admins_user = User.find(params[:id])
   end
 
   def update
+    @admins_user = User.find(params[:id])
+       if @admins_user.update(user_params)
+      redirect_to admins_user_path(@admins_user.id), notice: 'You have updated user successfully.'
+   else
+      render :edit
+   end
   end
 
+#destroyコントローラは不要と思われますが。。。残しておきます。
   def destroy
   end
 
-  def book_params
+  def user_params
     params.require(:user).permit(:email, :family_name_kanji, :first_name_kanji, :family_name_kana, :first_name_kana, :postal_code, :user_address, :tele_number, :leave_status)
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  enum leave_status: { alive: 0, dead: 1 }
 
    has_many :carts
    has_many :products, through: :carts
