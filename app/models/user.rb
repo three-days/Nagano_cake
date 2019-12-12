1c8d59830ffef8e680688aaf1345f7d747b87f3a
SAKURAI-S <japan.coffee.pm@gmail.com>
2019-12-12 10:50:19 +0000

    マイページ編集及びバリテーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable
 
   enum leave_status: { alive: 0, dead: 1 }
 
@@ -10,5 +10,18 @@ class User < ApplicationRecord
    has_many :products
 
   has_many :orders, dependent: :destroy
+
+
+
+  validates :email,uniqueness: true, presence: true
+  validates :family_name_kanji, presence: true
+  validates :first_name_kanji, presence: true
+  validates :family_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :postal_code, presence: true,format: {with: /\A\d{7}\z/}
+  validates :family_name_kana, presence: true,format: {with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力してください'}
+  validates :first_name_kana, presence: true,format: {with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力してください'}
+  validates :user_address, presence: true
+  validates :tele_number, presence: true
 
 end
