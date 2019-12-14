a0903c93be34e81997487a82b4511918fe992a39
hgfrtg <jrbevknf@outlook.com>
2019-12-14 16:51:20 +0900

    ジャンル検索機能追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,16 @@
 class ProductsController < ApplicationController
   def top
-  	@products = Product.all
-  	@genres = Genre.all.with_deleted
+  	@genres = Genre.all
+  # urlにgenre_id(params)がある場合
+  	if params[:genre_id]
+  		# Genreのデータベースのテーブルから一致するidを取得
+  		genre = Genre.find(params[:genre_id])
+  		# Genreのデータベースのテーブルから一致するidを取得
+  		@products = genre.products
+  	else
+  		 # 投稿すべてを取得
+  		@products = Product.all
+  	end
   end
   def create
     @product = Product.new(product_params)
