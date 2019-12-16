f27a2ea5c504950d4f49f9ff4157a9964f539f01
DAITADOKORO <bangasa0101@gmail.com>
2019-12-16 12:32:11 +0900

    finished deliverys => deliveries

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -1,48 +1,48 @@
 class DeliveriesController < ApplicationController
-	def show
-		@delivery = Delivery.select(current_user)
-		@deliveries = Delivery.all
-		@delivery = Delivery.new
-	end
+  def show
+    @delivery = Delivery.select(current_user)
+    @deliveries = Delivery.all
+    @delivery = Delivery.new
+  end
 
-	def new
-	end
+  def new
+  end
 
-	def create
-		@deliveries = Delivery.all
+  def create
+    @deliveries = Delivery.all
 
-		@delivery = Delivery.new(delivery_params)
-		@delivery.user_id = current_user.id
+    @delivery = Delivery.new(delivery_params)
+    @delivery.user_id = current_user.id
         if @delivery.save
-        			@delivery = Delivery.select(current_user)
+              @delivery = Delivery.select(current_user)
 
-		@delivery = Delivery.new
-        	redirect_to delivery_path(current_user)
+    @delivery = Delivery.new
+          redirect_to delivery_path(current_user)
         else
-        	render :show
+          render :show
         end
 
-	end
+  end
 
-	def edit
-		@deliveries = current_user
-	end
+  def edit
+    @deliveries = current_user
+  end
 
-	def update
-		@deliveries = current_user
-		@delivery.update(delivery_params)
-	end
+  def update
+    @deliveries = current_user
+    @delivery.update(delivery_params)
+  end
 
-	def update
-		@deliveries = current_user
-	end
+  def update
+    @deliveries = current_user
+  end
 
 
 private
-	  def delivery_params
-	  params.require(:delivery).permit(:delivery_address, :delivery_name, :delivery_postal_code)
+    def delivery_params
+    params.require(:delivery).permit(:delivery_address, :delivery_name, :delivery_postal_code)
 
-	end
+  end
 
 
 end
